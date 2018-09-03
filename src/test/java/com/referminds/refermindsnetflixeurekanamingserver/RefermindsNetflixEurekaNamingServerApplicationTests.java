e7a279aa92c7cca55f83def0b093892dd6d1a835
Rahul Sharma <rahulsharma.bbiet@gmail.com>
2018-09-03 14:23:32 +0300

    initial commit on github

---
+++ src/test/java/com/referminds/refermindsnetflixeurekanamingserver/RefermindsNetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.referminds.refermindsnetflixeurekanamingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefermindsNetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
